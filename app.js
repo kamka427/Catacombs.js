b88063e9ea85de183c4a99640a78b837024ada42
Kálmán Neszlényi <kamka42711@gmail.com>
2021-10-27 19:50:42 +0200

    simple map drawing,  basic rotate event

--- app.js
+++ app.js
@@ -10,22 +10,39 @@ const startmap = [
     ["random", "random", "random", "random", "random", "random", "random"],
     ["bottomleft", "random", "lineup", "random", "lineup", "random", "bottomright"],
 ];
-// let startmap: Array<Array<piece>> = [
-//     ["topleft", "random", "linedown", "random", "linedown", "random", "topright"],
-//     ["random","random", "random", "random", "random", "random", "random"],
-//     ["lineright", "random", "lineright", "random", "linedown", "random", "linevertical"],
-//     ["random","random", "random", "random", "random", "random", "random"],
-//     ["lineright", "random", "lineup", "random", "lineleft", "random", "linevertical"],
-//     ["random","random", "random", "random", "random", "random", "random"],
-//     ["bottomleft", "random", "lineup", "random", "lineup", "random", "bottomright"],
-// ]
-// let generateMap = () => generatedMap =  startmap.map(n=>n.map(e => e ==='random' ? pieceTypes[Math.floor(Math.random()*pieceTypes.length)] : e))
-// let generatedMap: Array<Array<piece>>  = generateMap()
-class map {
+class GameMap {
     constructor() {
         this.generateMap = () => startmap.map(e => e.map(e => e === 'random' ? pieceTypes[Math.floor(Math.random() * pieceTypes.length)] : e));
         this.map = this.generateMap();
         this.randomfield = pieceTypes[Math.floor(Math.random() * pieceTypes.length)];
     }
 }
-let x = new map();
+let gameMap = new GameMap();
+////////////////////////////////////////////////////////////////////////////
+const gameArea = document.querySelector("canvas#gameArea");
+const ctx = gameArea.getContext("2d");
+function drawMap() {
+    ctx.clearRect(0, 0, gameArea.width, gameArea.height);
+    for (let i = 0; i < gameMap.map.length + 2; i++) {
+        for (let j = 0; j < gameMap.map.length + 2; j++) {
+            if (i === 0 || j === 0 || i === gameMap.map.length + 1 || j === gameMap.map.length + 1) {
+                ctx.fillText("side", i * 50, j * 50 + 25);
+            }
+            else
+                ctx.fillText(gameMap.map[i - 1][j - 1], i * 50, j * 50 + 25);
+            ctx.strokeRect(i * 50, j * 50, 50, 50);
+        }
+    }
+    ctx.strokeRect(10 * 50, 0 * 50, 50, 50);
+    ctx.fillText(gameMap.randomfield, 10 * 50, 0 * 50 + 25);
+}
+drawMap();
+////////////////////////////////////////////////////////////////////////////
+function rotate(e) {
+    let x = Math.floor((gameMap.map.length * e.clientX) / (gameMap.map.length * 50)) - 1;
+    let y = Math.floor((gameMap.map.length * e.clientY) / (gameMap.map.length * 50)) - 1;
+    if (x >= 0 && x < gameMap.map.length && y >= 0 && y < gameMap.map.length)
+        gameMap.map[x][y] = 'lineup';
+    drawMap();
+}
+document.addEventListener("dblclick", rotate);
--- ts/map.ts
+++ ts/map.ts
@@ -2,31 +2,62 @@ type piece = "topleft" | "topright" | "bottomleft" | "bottomright" |
     "linevertical" | "linehorizontal" | "lineright" | "lineleft" | "lineup" |
     "linedown" | "random"
 
-const pieceTypes: Array<piece> = ["topleft" , "topright" , "bottomleft" , "bottomright" ,
-"linevertical" , "linehorizontal" , "lineright" , "lineleft" , "lineup" ,
-"linedown"]
+const pieceTypes: Array<piece> = ["topleft", "topright", "bottomleft", "bottomright",
+    "linevertical", "linehorizontal", "lineright", "lineleft", "lineup",
+    "linedown"]
 
 const startmap: Array<Array<piece>> = [
     ["topleft", "random", "linedown", "random", "linedown", "random", "topright"],
-    ["random","random", "random", "random", "random", "random", "random"],
+    ["random", "random", "random", "random", "random", "random", "random"],
     ["lineright", "random", "lineright", "random", "linedown", "random", "linevertical"],
-    ["random","random", "random", "random", "random", "random", "random"],
+    ["random", "random", "random", "random", "random", "random", "random"],
     ["lineright", "random", "lineup", "random", "lineleft", "random", "linevertical"],
-    ["random","random", "random", "random", "random", "random", "random"],
+    ["random", "random", "random", "random", "random", "random", "random"],
     ["bottomleft", "random", "lineup", "random", "lineup", "random", "bottomright"],
 ]
 
-class map{
+class GameMap {
     map: Array<Array<piece>>
     randomfield: piece
-    
-    
-    constructor(){
+
+    constructor() {
         this.map = this.generateMap()
-        this.randomfield = pieceTypes[Math.floor(Math.random()*pieceTypes.length)]
+        this.randomfield = pieceTypes[Math.floor(Math.random() * pieceTypes.length)]
     }
-    
-    generateMap = () => startmap.map(e=>e.map(e => e ==='random' ? pieceTypes[Math.floor(Math.random()*pieceTypes.length)] : e))
+
+    generateMap = () => startmap.map(e => e.map(e => e === 'random' ? pieceTypes[Math.floor(Math.random() * pieceTypes.length)] : e))
 }
 
-let x = new map();
+let gameMap: GameMap = new GameMap();
+
+////////////////////////////////////////////////////////////////////////////
+const gameArea: HTMLCanvasElement = document.querySelector("canvas#gameArea")
+const ctx: CanvasRenderingContext2D = gameArea.getContext("2d");
+
+function drawMap(): void {
+    ctx.clearRect(0,0,gameArea.width,gameArea.height);
+    for (let i = 0; i < gameMap.map.length+2; i++) {
+        for (let j = 0; j < gameMap.map.length+2; j++) {
+            if(i === 0 || j ===0 || i === gameMap.map.length+1 || j === gameMap.map.length+1) {
+                ctx.fillText("side", i * 50, j * 50 + 25)
+            }
+            else
+            ctx.fillText(gameMap.map[i-1][j-1], i * 50, j * 50 + 25)
+            ctx.strokeRect(i * 50, j * 50, 50, 50)
+        }
+    }
+    ctx.strokeRect(10 * 50, 0 * 50, 50, 50);
+    ctx.fillText(gameMap.randomfield, 10 * 50, 0 * 50 + 25);
+}
+drawMap()
+
+////////////////////////////////////////////////////////////////////////////
+function rotate(e: MouseEvent): void {
+    let x: number = Math.floor((gameMap.map.length * e.clientX) / (gameMap.map.length * 50)) -1
+    let y: number = Math.floor((gameMap.map.length * e.clientY) / (gameMap.map.length * 50)) -1
+    if(x >= 0 && x < gameMap.map.length && y >= 0 && y < gameMap.map.length)
+    gameMap.map[x][y] = 'lineup'
+    drawMap()
+}
+
+document.addEventListener("dblclick",rotate)
